912ad0c0d52d1769208c05a3b4f27bc069b06003
Amirjon Karimov <amirjonkarimoff@gmail.com>
2024-12-12 11:53:34 +0500

    deploy

--- django/users/admin.py
+++ django/users/admin.py
@@ -4,6 +4,7 @@ from .models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'username', 'user_id','is_blocked','created_date','updated_date']
+    list_filter = ['id', 'name', 'username', 'user_id','is_blocked']
 
 
 # Register your models here.
