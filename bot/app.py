9ffc0bb9708472e7dc223043213cb722b4eb4f93
amirjon <amirjon@amirjons-MacBook-Air.local>
2025-02-03 21:43:10 +0500

    deploy

--- bot/app.py
+++ bot/app.py
@@ -10,50 +10,6 @@ from random import choice
 from datetime import datetime
 import json
 
-# CHANNELS = {'@Amirjon_Karimov_Blog':523, '@Amirjon_Karimov_Life':113}
-# tugirlandi
-reactions = ["👍", "❤", "🔥", "🥰", "👏", "🎉", "🤩", "⚡", "👌", "😍", "❤‍🔥", "💯", "🤣", "🏆", "🍓", "🍾", "💋", "🎃", "😇", "🤝", "😘"]
-
-async def periodic_reaction(dp):
-    while True:
-        with open('channels_info.json','r') as f:
-            data = json.load(f)
-        channels = list(data.keys())
-        if channels:
-            for channel in channels:
-                try:
-                    reaction = f'{{"type": "emoji", "emoji": "{choice(reactions)}"}}'
-                    await dp.bot.request(
-                        method="setMessageReaction",
-                        data={
-                            "chat_id": channel,
-                            "message_id": data[channel]['message_id'],
-                            "reaction": f"[{reaction}]"
-                        }
-                    )
-                except Exception as e:
-                    await dp.bot.send_message(chat_id=ADMINS[0], text=f"Error: {e}:{reaction}")
-                    del data[channel]
-                    with open('channels_info.json', 'w') as file:
-                        json.dump(data, file, indent=4)
-                await asyncio.sleep(3)
-            
-            await asyncio.sleep(23)
-        else:
-            try:
-                reaction = f'{{"type": "emoji", "emoji": "{choice(reactions)}"}}'
-                await dp.bot.request(
-                    method="setMessageReaction",
-                    data={
-                        "chat_id": "@Amirjon_Karimov_Blog",
-                        "message_id": 500,
-                        "reaction": f"[{reaction}]"
-                    }
-                )
-            except Exception as e:
-                await dp.bot.send_message(chat_id=ADMINS[0], text=f"Error: {e}:{reaction}")
-            await asyncio.sleep(20)
-
 async def on_startup(dispatcher):
     # Birlamchi komandaPlar (/star va /help)
     await db.create()
@@ -67,7 +23,6 @@ async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
     # Bot ishga tushgani haqida adminga xabar berish
     await on_startup_notify(dispatcher)
-    asyncio.create_task(periodic_reaction(dp))
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
